8c21af3822d764cb7b5ca29c51e649311b86322c
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-01-30 13:38:18 +0000

    update

--- react-investment-calc/src/App.jsx
+++ react-investment-calc/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
       <Header />
       <UserInput />
-      <Output />
+      <Output initAmount annualInvestment returns duration />
     </main>
   );
 }
--- react-investment-calc/src/components/Output.jsx
+++ react-investment-calc/src/components/Output.jsx
@@ -1,25 +1,39 @@
 import { useState } from "react";
 
-export default function Output({ initAmount, annualInvestment, returns, duration }) {
-  const [finalAmount, setFinalAmount] = useState(0)
-
+export default function Output({
+  initAmount,
+  annualInvestment,
+  returns,
+  duration,
+}) {
+  const [finalAmount, setFinalAmount] = useState(0);
 
   const amounts = {
-    'Investment Value': initAmount * duration,
-    'Total Invested': initAmount + (annualInvestment * duration),
-    'Total Interest': 
+    "Investment Value": finalAmount,
+    "Total Invested": initAmount + annualInvestment * duration,
+    "Total Interest": finalAmount - (initAmount + annualInvestment * duration),
   };
 
-
   function calculateInterest() {
-    let total = initAmount
-    for (let i =0; i < duration; i++) {
+    let total = initAmount;
+    for (let i = 0; i < duration; i++) {
       total += annualInvestment;
-      total *= 1 + (returns/100)
+      total *= 1 + returns / 100;
     }
 
-    setFinalAmount(total)
+    setFinalAmount(total);
   }
 
-  return <div></div>;
+  return (
+    <div className="bg-blue-400 rounded text-white">
+      <div>
+        {Object.keys(amounts).map((amount, i) => {
+          <div key={i} className="bg-blue-400 flex flex-col gap-4">
+            <h2>{amount}</h2>
+            <p>{amounts[amount]}</p>
+          </div>;
+        })}
+      </div>
+    </div>
+  );
 }
